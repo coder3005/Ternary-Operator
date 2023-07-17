fb2562651df31169283f49dc8eda9161f0b832b7
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-17 11:03:31 +0530

    Create Ternary.java

---
+++ Ternary.java
@@ -0,0 +1,10 @@
+/* Ternary Operator Example...! */
+class Ternary
+{
+    public static void main(String[] args) {
+
+        int a=10,b=20,Max;
+        Max=(a>b) ? a : b;
+        System.out.print(Max);
+    }
+}
